5073f13456c935612a9e1c98c9d6f81767875ad6
kongzf <kongzf@126.com>
2018-12-21 21:59:08 +0800

    更改本地下载路径

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.csdn = "https://blog.csdn.net/qq_31866943"// csdn博客地址
-exports.output = "~/blog/source/_posts"// 这里可以定向到你的hexo源文件的地方，使用相对路径
-exports.base = "~/workspace/blog" // hexo博客源文件目录
+exports.output = "blog/source/_posts"// 这里可以定向到你的hexo源文件的地方，使用相对路径
+exports.base = "worspace/blog" // hexo博客源文件目录
 exports.overrideFiles = true // 文件已存在的情况下是否覆盖文件
 exports.getFirstPage = true // 是否只获取第1页数据（在第一次获取全量数据后使用这种方式获取最新数据）
